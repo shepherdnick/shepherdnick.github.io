969104f31f82337a8bbfb43adf5e6753284e6eaa
shepherdnick <nick.shepherd@ws11.sureviewinternational.com>
2016-10-10 09:05:31 +0100

    Added unplayed

---
+++ unplayed/libs/unplayed.js
@@ -0,0 +1,110 @@
+window.onload = startGui;
+function startGui(){
+	// make a new javascript markdown converter
+	converter = new Showdown.converter();
+	
+	// html holding vars
+	var unplayedGames = null;
+	var unbeatenGames = null;
+	var beatenGames = null;
+	var multiplayerGames = null;
+	
+	// create the html markdown, and then swap the () for spans
+	unplayedGames = spanIt(converter.makeHtml(getData("unplayed.markdown")));
+	unbeatenGames = spanIt(converter.makeHtml(getData("unbeaten.markdown")));
+	beatenGames = spanIt(converter.makeHtml(getData("beaten.markdown")));
+	multiplayerGames = spanIt(converter.makeHtml(getData("multiplayer.markdown")));	
+	
+	// add the new html blocks to the DOM
+	$("#unplayed").html(unplayedGames);
+	$("#unbeaten").html(unbeatenGames);
+	$("#beaten").html(beatenGames);
+	$("#multiplayer").html(multiplayerGames);
+	
+	// add star ratings to the abandoned and the beaten lists
+	starIt("multiplayer");
+	starIt("beaten");
+	
+	// if we don't call the star-rating api after the html has changed, all we get is radio buttons - yeuch
+	$('input[type=radio].star').rating();
+}
+
+function spanIt(rawHtml)
+{
+	// backup the raw html
+	var changedHtml = rawHtml;
+	// replace the brackets
+	changedHtml = changedHtml.replace(/\(/g, "<span>");
+	changedHtml = changedHtml.replace(/\)/g, "</span>");
+	// send the shiny new html back
+	return changedHtml;
+}
+
+function starIt(holdingId)
+{
+	// find the right DOM element
+	var html = "";
+	var selector = "#" + holdingId;	
+	var $column = $(selector);
+	
+	// for each list item in the DOM element (every game in a column)
+	$column.find('li').each(function (index){
+		var htmlBlock = $(this).html();
+		// try to find the star rating e.g. [3/5]
+		var allStarRatings = htmlBlock.match(/\[\d\/\d\]/g);
+		
+		// get the number of stars we want to give the game
+        var starRating = /\[(\d)\/(\d)\]/g;
+		if(allStarRatings != null && typeof(allStarRatings) != 'undefined')
+		{
+			if(allStarRatings.length > 0){
+				var match = starRating.exec(allStarRatings[0]); //regex match
+				
+				html += "<div class='starRatingHolder'>";
+				
+				// create the inputs based on the number of stars we want to give
+				for(var i = 1; i <= parseInt(match[2]); i++)
+				{
+					if(i == parseInt(match[1]))
+					{
+						// this one is checked to show it's where the stars should stop
+						html += "<input name='star-rating-" + holdingId + "-" + index + "' type='radio' class='star' disabled='disabled' checked='checked'\/>";
+					}
+					else{
+						html += "<input name='star-rating-" + holdingId + "-" + index + "' type='radio' class='star' disabled='disabled'\/>";
+					}
+				}
+				
+				html += "</div>";
+			}
+		}
+		
+		// actually remove the [3/5] from the page
+		htmlBlock = htmlBlock.replace(/\[\d\/\d\]/g, "");
+		htmlBlock += html;
+		
+		// add the new html to the DOM
+		$(this).html(htmlBlock);
+		
+		// blank down the vars we used
+		html = "";
+		htmlBlock = "";
+	});
+}
+
+function getData(nameOfFile)
+{
+	var markdownData = null;
+	
+	// use jQuery ajax to get the markdown file
+	$.ajax({
+     async: false,
+     type: 'GET',
+     url: 'https://dl.dropboxusercontent.com/u/2013399/unplayed/' + nameOfFile,
+     success: function(data) {
+          markdownData = data;
+		}
+	});
+
+	return markdownData;
+}
---
+++ unplayed/unbeaten.markdown
@@ -0,0 +1,5 @@
+But not abandoned.
+
+- Warbits (iOS)
+- Lara Croft: GO (iOS)
+- Yoshi's Woolly World (WiiU)(Bloody loved playing this game on Christie's Wii U)

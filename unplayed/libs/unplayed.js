f76fe66dd046d568aae8d8c8a156a275b96dbdf3
shepherdnick <nick@shepherdnickbook.local>
2018-06-03 21:23:49 +0100

    Changed showdown to get the list

--- unplayed/libs/unplayed.js
+++ unplayed/libs/unplayed.js
@@ -2,17 +2,17 @@ window.onload = startGui;
 function startGui(){
 	// make a new javascript markdown converter
 	converter = new Showdown.converter();
-	
+
 	// Get all the data from dropbox
 	getData("rb6q39iwuoscnwa", "unplayed.markdown");
 	getData("5gnpuuzx3tupojm", "unbeaten.markdown");
 	getData("x960zbogwo10odp", "completed.markdown");
 	getData("z55s0jmyb7a2cfg", "multiplayer.markdown");
-	
+
 	// add star ratings to the abandoned and the beaten lists
 	starIt("multiplayer");
 	starIt("completed");
-	
+
 	// if we don't call the star-rating api after the html has changed, all we get is radio buttons - yeuch
 	$('input[type=radio].star').rating();
 }
@@ -32,24 +32,24 @@ function starIt(holdingId)
 {
 	// find the right DOM element
 	var html = "";
-	var selector = "#" + holdingId;	
+	var selector = "#" + holdingId;
 	var $column = $(selector);
-	
+
 	// for each list item in the DOM element (every game in a column)
 	$column.find('li').each(function (index){
 		var htmlBlock = $(this).html();
 		// try to find the star rating e.g. [3/5]
 		var allStarRatings = htmlBlock.match(/\[\d\/\d\]/g);
-		
+
 		// get the number of stars we want to give the game
         var starRating = /\[(\d)\/(\d)\]/g;
 		if(allStarRatings != null && typeof(allStarRatings) != 'undefined')
 		{
 			if(allStarRatings.length > 0){
 				var match = starRating.exec(allStarRatings[0]); //regex match
-				
+
 				html += "<div class='starRatingHolder'>";
-				
+
 				// create the inputs based on the number of stars we want to give
 				for(var i = 1; i <= parseInt(match[2]); i++)
 				{
@@ -62,18 +62,18 @@ function starIt(holdingId)
 						html += "<input name='star-rating-" + holdingId + "-" + index + "' type='radio' class='star' disabled='disabled'\/>";
 					}
 				}
-				
+
 				html += "</div>";
 			}
 		}
-		
+
 		// actually remove the [3/5] from the page
 		htmlBlock = htmlBlock.replace(/\[\d\/\d\]/g, "");
 		htmlBlock += html;
-		
+
 		// add the new html to the DOM
 		$(this).html(htmlBlock);
-		
+
 		// blank down the vars we used
 		html = "";
 		htmlBlock = "";
@@ -83,7 +83,7 @@ function starIt(holdingId)
 function dataCalledBack(nameOfFile, data)
 {
 	switch(nameOfFile)
-	{	
+	{
 		case "unplayed.markdown":
 			var unplayedGames = spanIt(converter.makeHtml(data));
 			$("#unplayed").html(unplayedGames);
@@ -106,7 +106,7 @@ function dataCalledBack(nameOfFile, data)
 			alert("No data found!");
 			break;
 	}
-	
+
 	// if we don't call the star-rating api after the html has changed, all we get is radio buttons - yeuch
 	$('input[type=radio].star').rating();
 }
@@ -128,7 +128,7 @@ function createCORSRequest(method, url){
 function getData(shareId, nameOfFile)
 {
 	var markdownData = null;
-	
+
 	var request = createCORSRequest( "get", "https://dl.dropboxusercontent.com/s/" + shareId + "/" + nameOfFile + "?dl=1" );
 	if ( request ){
   		// Define a callback function

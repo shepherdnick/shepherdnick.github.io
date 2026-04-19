c31d460569e382c1c87cf8897aa86f563ab3e064
Nick Shepherd <2045298+shepherdnick@users.noreply.github.com>
2026-04-19 14:32:42 +0100

    Updated bracket type.

--- unplayed/libs/unplayed.js
+++ unplayed/libs/unplayed.js
@@ -21,8 +21,8 @@ function spanIt(rawHtml) {
 	// backup the raw html
 	var changedHtml = rawHtml;
 	// replace the brackets
-	changedHtml = changedHtml.replace(/\#\(/g, "<span class='tag'>");
-	changedHtml = changedHtml.replace(/\#\)/g, "</span>");
+	changedHtml = changedHtml.replace(/\{/g, "<span class='tag'>");
+	changedHtml = changedHtml.replace(/\})/g, "</span>");
 	changedHtml = changedHtml.replace(/\(/g, "<span");
 	changedHtml = changedHtml.replace(/\)/g, "</span>");
 	// send the shiny new html back
